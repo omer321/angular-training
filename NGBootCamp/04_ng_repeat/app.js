ff90e990d4c795d70801349fa669fd8d14952780
joelhooks <joelhooks@gmail.com>
2014-12-08 13:22:43 -0800

    basic use of ng-repeat

--- NGBootCamp/04_ng_repeat/app.js
+++ NGBootCamp/04_ng_repeat/app.js
@@ -2,6 +2,22 @@ angular.module('prosper-workshop', [])
   .controller('AppCtrl', function AppCtrl() {
     var appCtrl = this;
 
+    var same = {name: 'Dixie'}
+
+    appCtrl.people = [
+      {name: 'Fred'},
+      {name: 'Gomer'},
+      {name: 'Gomer'},
+      {name: 'Linda'}
+    ];
+
+    appCtrl.morePeople = [
+      same,
+      same,
+      {name: 'Jasper'},
+      {name: 'Flo'},
+      {name: 'Reggie'}
+    ]
   })
 ;
 
