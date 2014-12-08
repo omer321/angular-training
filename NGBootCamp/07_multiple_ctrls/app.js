b3344be229633ce0ffbb16dd570f67df2942acce
joelhooks <joelhooks@gmail.com>
2014-12-08 14:26:16 -0800

    multiple controllers

--- NGBootCamp/07_multiple_ctrls/app.js
+++ NGBootCamp/07_multiple_ctrls/app.js
@@ -2,6 +2,19 @@ angular.module('prosper-workshop', [])
   .controller('AppCtrl', function AppCtrl() {
     var appCtrl = this;
 
+    appCtrl.message = "This is an AppCtrl message."
+  })
+
+  .controller('AdminCtrl', function AdminCtrl() {
+    var adminCtrl = this;
+
+    adminCtrl.message = "This is an AdminCtrl message";
+  })
+
+  .controller('DashCtrl', function DashCtrl() {
+    var dashCtrl = this;
+
+    dashCtrl.message = "This is an DashCtrl message";
   })
 ;
 
