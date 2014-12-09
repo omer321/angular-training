3b242f0c7be80c05b11134a0cb838326ac0d1a50
joelhooks <joelhooks@gmail.com>
2014-12-09 12:58:17 -0800

    show an example of using a factory with the PersonModel constructor

--- project-seed/src/app/app.js
+++ project-seed/src/app/app.js
@@ -14,15 +14,35 @@ angular.module('project-seed', [
 
     peopleModel.getPeople().then(function(people) {
       app.people = people;
+
+      _.each(people, function(person) {
+        person.sayHello();
+      })
     })
   })
 
-  .service('peopleModel', function PeopleModel($http) {
+  .factory('PersonModel', function($http) {
+    function PersonModel(name) {
+      this.name = name
+    }
+
+    PersonModel.prototype.sayHello = function() {
+      console.log('HELLO!', this.name);
+    };
+
+    return PersonModel;
+  })
+
+  .service('peopleModel', function PeopleModel($http, PersonModel) {
     var peopleModel = this;
 
     peopleModel.getPeople = function() {
       return $http.get('http://www.json-generator.com/api/json/get/bQqreHpQwO?indent=2').then(function (results) {
-        peopleModel.people = results.data;
+        peopleModel.people = [];
+        _.each(results.data, function(person) {
+          var newPerson = new PersonModel(person.name);
+          peopleModel.people.push(newPerson);
+        });
         return peopleModel.people;
       })
     }
