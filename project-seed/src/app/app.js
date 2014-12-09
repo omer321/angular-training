fc1166d940bd67bdffacb30db3d85b16c62b74e3
joelhooks <joelhooks@gmail.com>
2014-12-09 13:04:28 -0800

    reset for github example

--- project-seed/src/app/app.js
+++ project-seed/src/app/app.js
@@ -7,44 +7,8 @@ angular.module('project-seed', [
   'ngSanitize'
 ])
 
-  .controller('AppCtrl', function AppCtrl(peopleModel) {
+  .controller('AppCtrl', function AppCtrl() {
     var app = this;
 
-    app.loading = true;
-
-    peopleModel.getPeople().then(function(people) {
-      app.people = people;
-
-      _.each(people, function(person) {
-        person.sayHello();
-      })
-    })
-  })
-
-  .factory('PersonModel', function($http) {
-    function PersonModel(name) {
-      this.name = name
-    }
-
-    PersonModel.prototype.sayHello = function() {
-      console.log('HELLO!', this.name);
-    };
-
-    return PersonModel;
-  })
-
-  .service('peopleModel', function PeopleModel($http, PersonModel) {
-    var peopleModel = this;
-
-    peopleModel.getPeople = function() {
-      return $http.get('http://www.json-generator.com/api/json/get/bQqreHpQwO?indent=2').then(function (results) {
-        peopleModel.people = [];
-        _.each(results.data, function(person) {
-          var newPerson = new PersonModel(person.name);
-          peopleModel.people.push(newPerson);
-        });
-        return peopleModel.people;
-      })
-    }
   })
 ;
