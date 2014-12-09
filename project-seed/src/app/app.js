743d6a266ed72bfeb1c2ee58ff9ae7958a45d80f
joelhooks <joelhooks@gmail.com>
2014-12-09 11:40:41 -0800

    use the json generator to grab some mock people

--- project-seed/src/app/app.js
+++ project-seed/src/app/app.js
@@ -1,13 +1,30 @@
 angular.module('project-seed', [
   'project-seed.common',
   'ui.router',
-  'templates-app',
+  //'templates-app',
   'ngAria',
   'angularMoment',
   'ngSanitize'
 ])
 
-  .controller('AppCtrl', function AppCtrl($state) {
+  .controller('AppCtrl', function AppCtrl(peopleModel) {
     var app = this;
+
+    app.loading = true;
+
+    peopleModel.getPeople().then(function(people) {
+      app.people = people;
+    })
+  })
+
+  .service('peopleModel', function PeopleModel($http) {
+    var peopleModel = this;
+
+    peopleModel.getPeople = function() {
+      return $http.get('http://www.json-generator.com/api/json/get/bQqreHpQwO?indent=2').then(function (results) {
+        peopleModel.people = results.data;
+        return peopleModel.people;
+      })
+    }
   })
 ;
